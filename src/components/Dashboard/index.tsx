754b805dbcdd2afda74336128361e28c3b091872
TDAHung <tdahung260902@gmail.com>
2024-06-10 00:35:33 +0700

    fix "get all messages even not user's messages"

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header"
 import { Button, FloatButton, Form, Input } from "antd";
 import { CustomerServiceOutlined } from '@ant-design/icons';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { AuthContext } from "../../contexts/authContext";
 import { Loading } from "../Loading";
@@ -24,8 +24,8 @@ const GET_MESSAGES = gql`
 
 const Dashboard = () => {
     const [messagesRender, setMessagesRender] = useState<any>([]);
-
     const [message, setMessage] = useState('');
+    const messagesEndRef = useRef<HTMLDivElement>(null);
 
     const user = useContext(AuthContext);
     const messages = useQuery(GET_MESSAGES, {
@@ -36,16 +36,22 @@ const Dashboard = () => {
 
     useEffect(() => {
         if (!messages.loading && !messages.error) {
-            setMessagesRender(messages.data.messages)
+            setMessagesRender(messages.data.messages);
         }
-    }, [messages.loading, messages.error])
+    }, [messages.loading, messages.error]);
 
     useEffect(() => {
-        socket.on('onMessage', (payload) => {
-            console.log(payload.content);
-            setMessagesRender((prev: any) => [...prev, payload.content])
-        })
-    }, [])
+        const handleMessage = (payload: any) => {
+            if (payload.content.receiverId === user.id || payload.content.senderId === user.id) {
+                setMessagesRender((prev: any) => [...prev, payload.content]);
+            }
+        };
+
+        socket.on('onMessage', handleMessage);
+        return () => {
+            socket.off('onMessage', handleMessage);
+        };
+    }, [user.id]);
 
     const ChatMessage = ({ message, isSender }: any) => {
         const options = {
@@ -81,41 +87,54 @@ const Dashboard = () => {
         setMessage('');
         socket.emit('message', JSON.stringify(messageToSend));
     }
-    return <main>
-        <Header />
-        <div className="container mx-auto p-4 px-20">
-            <Outlet />
-        </div>
-        <FloatButton.Group
-            trigger="click"
-            type="primary"
-            style={{ left: 24 }}
-            icon={<CustomerServiceOutlined />}
-        >
-            <div className="m-4 bg-white rounded-lg shadow-lg" style={{ height: "50rem", width: "40rem" }}>
-                <div className="bg-red-600 text-white p-4 rounded-t-lg">
-                    <h2 className="text-2xl">Chat with us</h2>
-                </div>
-                <div className="p-4 flex justify-between flex-col" style={{ height: "46rem" }}>
-                    <div className="space-y-4 overflow-auto">
-                        {
-                            messages.error || messages.loading ? <Loading /> :
-                                messagesRender.map((message: any, index: number) => (
-                                    <ChatMessage key={index} message={message} isSender={message.senderId} />
-                                ))}
-                    </div>
-                    <Form layout="vertical" className="mt-2">
-                        <div className="flex">
-                            <Input placeholder="message" value={message} onChange={(value) => { setMessage(value.currentTarget.value) }} />
-                            <Button className="bg-red-600 text-white" onClick={handleSendMessage}>
-                                Send
-                            </Button>
-                        </div>
-                    </Form>
-                </div>
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messagesRender]);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
+        <main>
+            <Header />
+            <div className="container mx-auto p-4 px-20">
+                <Outlet />
             </div>
-        </FloatButton.Group>
-    </main >
+            <FloatButton.Group
+                trigger="click"
+                type="primary"
+                style={{ left: 24 }}
+                icon={<CustomerServiceOutlined />}
+            >
+                <div className="m-4 bg-white rounded-lg shadow-lg" style={{ height: "50rem", width: "40rem" }}>
+                    <div className="bg-red-600 text-white p-4 rounded-t-lg">
+                        <h2 className="text-2xl">Chat with us</h2>
+                    </div>
+                    <div className="p-4 flex justify-between flex-col" style={{ height: "46rem" }}>
+                        <div className="space-y-4 overflow-auto">
+                            {
+                                messages.error || messages.loading ? <Loading /> :
+                                    messagesRender.map((message: any, index: number) => (
+                                        <ChatMessage key={index} message={message} isSender={message.senderId} />
+                                    ))
+                            }
+                            <div ref={messagesEndRef} />
+                        </div>
+                        <Form layout="vertical" className="mt-2">
+                            <div className="flex">
+                                <Input placeholder="message" value={message} onChange={(value) => { setMessage(value.currentTarget.value) }} />
+                                <Button className="bg-red-600 text-white" onClick={handleSendMessage}>
+                                    Send
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </FloatButton.Group>
+        </main>
+    )
 }
 
 export default Dashboard;
